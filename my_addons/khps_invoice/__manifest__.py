e5dff100db9c90dd2ede93eccf8405869ff0ea8b
mlopesbjj <mlopesbjj@gmail.com>
2024-10-25 18:15:51 +0200

    [ADD][KHPS_INVOICE] modulo custom per gli ordini.

---
+++ my_addons/khps_invoice/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "KHPS Invoice",
+    "description": "KHPS Invoice",
+    "summary": "KHPS Invoice",
+    "author": "Marcio Lopes - KHPS",
+    "license": "AGPL-3",
+    "version": "16.0.0.0.0",
+    "installable": True,
+    "depends": ['base', 
+                'sale'],
+    "data": []
+}
